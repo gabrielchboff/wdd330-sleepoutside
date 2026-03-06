3071a0d0ef3b8704aa8b617cab700649b4befe13
gabrielchboff <gabrielchagasboff@gmail.com>
2026-03-06 17:00:53 -0300

    FIX: Cart add itens

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cart = getLocalStorage("so-cart") || [];
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -13,6 +15,7 @@ async function addToCartHandler(e) {
 }
 
 // add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const addToCartButton = document.getElementById("addToCart");
+if (addToCartButton) {
+  addToCartButton.addEventListener("click", addToCartHandler);
+}
