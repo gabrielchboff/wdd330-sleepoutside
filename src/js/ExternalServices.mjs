c6fb48856ad3ab50a9d245459c52b9b0c0dbc5c6
gabrielchboff <gabrielchagasboff@gmail.com>
2026-03-28 18:56:10 -0300

    fix: requests

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,5 +1,5 @@
 const baseURL =
-  import.meta.env.VITE_SERVER_URL || "https://wdd330-backend.onrender.com/";
+  import.meta.env.VITE_SERVER_URL || "https://wdd330-backend.onrender.com";
 
 async function convertToJson(res) {
   const jsonResponse = await res.json();
@@ -12,12 +12,12 @@ async function convertToJson(res) {
 
 export default class ExternalServices {
   async getData(category) {
-    const response = await fetch(`${baseURL}products/search/${category}`);
+    const response = await fetch(`${baseURL}/products/search/${category}`);
     const data = await convertToJson(response);
     return data.Result;
   }
   async findProductById(id) {
-    const response = await fetch(`${baseURL}product/${id}`);
+    const response = await fetch(`${baseURL}/product/${id}`);
     const data = await convertToJson(response);
     return data.Result;
   }
@@ -27,7 +27,7 @@ export default class ExternalServices {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload),
     };
-    const response = await fetch(`${baseURL}checkout`, options);
+    const response = await fetch(`${baseURL}/checkout`, options);
     return await convertToJson(response);
   }
 }
